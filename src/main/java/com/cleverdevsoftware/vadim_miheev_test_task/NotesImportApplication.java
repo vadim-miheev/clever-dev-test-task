c9a1c57bd4f6170e34520ec377ee9a3c3ec7b028
Vadim Miheev <mnixav@gmail.com>
2022-11-30 21:58:52 +0300

    Application entrypoint;

---
+++ src/main/java/com/cleverdevsoftware/vadim_miheev_test_task/NotesImportApplication.java
@@ -0,0 +1,11 @@
+package com.cleverdevsoftware.vadim_miheev_test_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotesImportApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NotesImportApplication.class, args);
+    }
+}
