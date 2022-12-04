a296b4bf4cab54f6be0c2348d4fecc6440acf71b
Vadim Miheev <mnixav@gmail.com>
2022-12-04 19:05:50 +0300

    run-import-after-start setting;

--- src/main/java/com/cleverdevsoftware/test_task/vadim_miheev/notes_import_system/NotesImportApplication.java
+++ src/main/java/com/cleverdevsoftware/test_task/vadim_miheev/notes_import_system/NotesImportApplication.java
@@ -1,11 +1,17 @@
 package com.cleverdevsoftware.test_task.vadim_miheev.notes_import_system;
 
+import com.cleverdevsoftware.test_task.vadim_miheev.notes_import_system.service.ScheduledImportService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class NotesImportApplication {
     public static void main(String[] args) {
-        SpringApplication.run(NotesImportApplication.class, args);
+        ApplicationContext app = SpringApplication.run(NotesImportApplication.class, args);
+        if (Boolean.parseBoolean(app.getBean(Environment.class).getProperty("app.run-import-after-start", "false"))) {
+            app.getBean(ScheduledImportService.class).start();
+        }
     }
 }
